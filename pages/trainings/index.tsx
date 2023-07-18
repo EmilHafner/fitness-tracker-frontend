9eb507424fd0bef984a666bccb3e7ec10c98ec67
Aronlukas <hafner.emil@gmail.com>
2023-07-18 12:30:34 +0200

    Training only accessible for authenticated users. Started working on AddTrainingComponent.

--- pages/trainings/index.tsx
+++ pages/trainings/index.tsx
@@ -1,41 +1,55 @@
-import { useEffect, useState } from "react";
-import { getAllTrainings } from "@/services/axiosInstance";
+import {useEffect, useState} from "react";
+import {getAllTrainings} from "@/services/axiosInstance";
 import TrainingItem, {
-  TrainingItemInterface,
+    TrainingItemInterface,
 } from "@/components/training/TrainingItem";
-import Router from "next/router";
-import { useToast } from "@chakra-ui/react";
-import { useSession } from "next-auth/react";
+import {useToast} from "@chakra-ui/react";
+import {signIn, useSession} from "next-auth/react";
+import AddTrainingComponent from "@/components/training/AddTrainingComponent";
 
 export default function Trainings() {
-  const [trainings, setTrainings] = useState([]);
-  const toast = useToast();
-  const { data: session } = useSession();
+    const [trainings, setTrainings] = useState([]);
+    const toast = useToast();
+    const {data: session} = useSession({
+        required: true,
+        onUnauthenticated() {
+            toast({
+                title: "Cannot access this page",
+                description: "You have to login first.",
+                status: "error",
+                variant: "left-accent",
+                duration: 4000,
+                isClosable: true,
+            });
+            signIn();
+        }
+    });
 
-  useEffect(() => {
-    getAllTrainings()
-      .then((res) => {
-        setTrainings(res.data);
-      })
-      .catch(() => {
-        toast({
-          title: "Cannot access this page",
-          description: "You have to login first.",
-          status: "error",
-          variant: "left-accent",
-          duration: 4000,
-        });
-        Router.push("/auth/login");
-      });
-  }, [toast]);
+    useEffect(() => {
+        if (!session) return;
+        getAllTrainings()
+            .then((res) => {
+                setTrainings(res.data);
+            }).catch((e) => toast({
+            title: "An Error occured",
+            description: e.message,
+            status: "error",
+            variant: "left-accent",
+            duration: 4000,
+            isClosable: true,
+        }))
+    }, [toast]);
 
-  return (
-    <div
-      className={"flex flex-col-reverse items-center gap-4 pt-4 min-h-screen"}
-    >
-      {trainings.map((training: TrainingItemInterface, i) => (
-        <TrainingItem key={i} {...training} />
-      ))}
-    </div>
-  );
+    if (!session) return null;
+
+    return (
+        <div
+            className={"flex flex-col-reverse items-center gap-4 pt-4 min-h-screen"}
+        >
+            {trainings.map((training: TrainingItemInterface, i) => (
+                <TrainingItem key={i} {...training} />
+            ))}
+            <AddTrainingComponent/>
+        </div>
+    );
 }
