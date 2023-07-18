ac3d51cf205ed2190d145c824e3e758728e28259
Aronlukas <hafner.emil@gmail.com>
2023-07-18 23:14:43 +0200

    Added an abort color

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,26 +1,28 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: [
-        './pages/**/*.{js,ts,jsx,tsx,mdx}',
-        './components/**/*.{js,ts,jsx,tsx,mdx}',
-        './app/**/*.{js,ts,jsx,tsx,mdx}',
-    ],
-    theme: {
-
-        extend: {
-            fontFamily: {
-                sans: ['Inter', 'sans-serif']
-            },
-            colors: { // Default dark fitness-related theme
-                'background': '#feffff',
-                'primary-dark': '#2c3531',
-                'primary': '#176e70',
-                'text': '#070707',
-                'accent': '#ffcb9a',
-                'accent-muted': '#d5ac88',
-                'accent-light': '#d1e8e2'
-            }
-        },
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Inter", "sans-serif"],
+      },
+      colors: {
+        // Default dark fitness-related theme
+        background: "#feffff",
+        "primary-dark": "#2c3531",
+        primary: "#176e70",
+        text: "#070707",
+        accent: "#ffcb9a",
+        "accent-muted": "#d5ac88",
+        "accent-light": "#d1e8e2",
+        abort: "#f1634f",
+        "abort-muted": "#e14135",
+      },
     },
-    plugins: [],
-}
+  },
+  plugins: [],
+};
