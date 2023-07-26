f3c01d6e8e5eee007958e4cef665fe57a490d802
Aronlukas <hafner.emil@gmail.com>
2023-07-26 20:22:12 +0200

    Changes to custom button component. * Added loading-animation * Added big variant

--- components/basics/Button.tsx
+++ components/basics/Button.tsx
@@ -1,19 +1,34 @@
+import { SpinnerIcon } from "@chakra-ui/icons";
+import { useEffect } from "react";
 import { twMerge } from "tailwind-merge";
 
 type ButtonProps = {
-  className?: string;
+  className?: string,
+  variant?: "normal" | "big",
+  isLoading?: boolean,
   children: React.ReactNode,
 } & React.ButtonHTMLAttributes<HTMLButtonElement>;
 
 export default function Button(props: ButtonProps) {
+  const { className, variant, isLoading, children, ...rest } = props;
+
+  let sizeClasses = "px-2 py-2 ";
+  if (props.variant === "big") {
+    sizeClasses = "px-6 py-4 "
+  }
+
   return (
-    <button {...props}
-    className={twMerge(
-        "px-4 py-2 rounded-xl shadow-xl bg-accent hover:bg-accent-muted",
-        props.className
+    <button {...rest}
+    className={twMerge(sizeClasses + (rest.disabled && "opacity-50 cursor-not-allowed") +
+        " rounded-xl shadow-xl bg-accent hover:bg-accent-muted font-medium relative",
+        className
       )}
     >
-      {props.children}
+
+      {isLoading && (<div className={"absolute w-full h-full flex justify-center items-center left-0 top-0"}>
+        <SpinnerIcon className="animate-spin h-5 w-5"></SpinnerIcon>
+      </div>)}
+      <span className={isLoading ? "opacity-0" : "opacity-100"}>{children}</span>
     </button>
   );
 }
