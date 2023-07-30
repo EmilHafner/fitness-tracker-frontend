bc41cb8a19df635f3f098333d1d44320b83e2289
Aronlukas <hafner.emil@gmail.com>
2023-07-31 00:00:08 +0200

    Some stile changes to the custom button

--- components/basics/Button.tsx
+++ components/basics/Button.tsx
@@ -3,32 +3,35 @@ import { useEffect } from "react";
 import { twMerge } from "tailwind-merge";
 
 type ButtonProps = {
-  className?: string,
-  variant?: "normal" | "big",
-  isLoading?: boolean,
-  children: React.ReactNode,
+    className?: string;
+    variant?: "normal" | "big";
+    isLoading?: boolean;
+    children: React.ReactNode;
 } & React.ButtonHTMLAttributes<HTMLButtonElement>;
 
 export default function Button(props: ButtonProps) {
-  const { className, variant, isLoading, children, ...rest } = props;
+    const { className, variant, isLoading, children, disabled, ...rest } = props;
 
-  let sizeClasses = "px-2 py-2 ";
-  if (props.variant === "big") {
-    sizeClasses = "px-6 py-4 "
-  }
+    let sizeClasses = "px-2 py-2 ";
+    if (props.variant === "big") {
+        sizeClasses = "px-6 py-4 ";
+    }
 
-  return (
-    <button {...rest}
-    className={twMerge(sizeClasses + (rest.disabled && "opacity-50 cursor-not-allowed") +
-        " rounded-xl shadow-xl bg-accent hover:bg-accent-muted font-medium relative",
-        className
-      )}
-    >
+    return (
+        <button
+            {...rest}
+            className={twMerge(
+                sizeClasses +
+                    (disabled && "cursor-not-allowed opacity-50") +
+                    "relative rounded-xl bg-accent font-medium shadow-xl hover:bg-accent-muted",
+                className
+            )}
+        >
+            <div className={(!isLoading && "opacity-0 ") + "absolute flex h-full w-full items-center justify-center"}>
+                <SpinnerIcon className="absolute animate-spin"></SpinnerIcon>
+            </div>
 
-      {isLoading && (<div className={"absolute w-full h-full flex justify-center items-center left-0 top-0"}>
-        <SpinnerIcon className="animate-spin h-5 w-5"></SpinnerIcon>
-      </div>)}
-      <span className={isLoading ? "opacity-0" : "opacity-100"}>{children}</span>
-    </button>
-  );
+            <span className={isLoading ? "opacity-0" : "opacity-100"}>{children}</span>
+        </button>
+    );
 }
