547fd75f168fdef658e23a3ec7c1ba6b14b6b342
Aronlukas <hafner.emil@gmail.com>
2023-07-31 00:02:57 +0200

    Started working on training view. * Added passed time (problems with time zone) * Gets exerciseEvents for each training and displays the exerciseType

--- pages/trainings/[id].tsx
+++ pages/trainings/[id].tsx
@@ -1,122 +1,174 @@
 import { axiosI } from "@/services/axiosInstance";
-import { AddIcon } from "@chakra-ui/icons";
+import { AddIcon, TimeIcon } from "@chakra-ui/icons";
 import { Skeleton, useToast } from "@chakra-ui/react";
-import { format } from "date-fns";
+import {
+    differenceInMilliseconds,
+    differenceInSeconds,
+    format,
+    formatDistanceToNow,
+    formatDistanceToNowStrict,
+    getMilliseconds,
+} from "date-fns";
 import { useRouter } from "next/router";
-import { useEffect, useState, useCallback } from "react";
+import { useEffect, useState, useCallback, useMemo } from "react";
 
 interface Set {
-  id: number;
-  weight?: number;
-  reps?: number;
+    id: number;
+    weight?: number;
+    reps?: number;
 }
-interface Exercise {
-  id: number;
-  name: string;
-  bodypart?: string;
-  sets?: Array<Set>;
+interface ExerciseType {
+    id: number;
+    name: string;
+    bodypart?: string;
 }
+
+interface ExerciseEvent {
+    id: number;
+    exerciseType: ExerciseType;
+    sets?: Array<Set>;
+}
+
 interface Training {
-  id: number;
-  startDateTime: Date;
-  endDateTime: Date;
-  exercises?: Array<Exercise>;
+    id: number;
+    startDateTime: Date;
+    endDateTime: Date;
+    exerciseEvents?: Array<ExerciseEvent>;
 }
 
 export default function Training() {
-  const router = useRouter();
-  const [training, setTraining] = useState({} as Training);
-  const [isLoading, setIsLoading] = useState(true);
-  const toast = useToast();
-  const [loadNewExercise, setLoadNewExercise] = useState(false);
+    const router = useRouter();
+    const [training, setTraining] = useState({} as Training);
+    const [isLoading, setIsLoading] = useState(true);
+    const toast = useToast();
+    const [loadNewExercise, setLoadNewExercise] = useState(false);
+    const [trainingActive, setTrainingActive] = useState(false);
+    const [exerciseEvents, setExerciseEvents] = useState({} as ExerciseEvent[]);
 
+    // Load training from API
+    const loadTraining = useCallback(() => {
+        return axiosI
+            .get(`/training/${router.query.id}`)
+            .then((res) => {
+                setTraining(res.data);
+            })
+            .catch((e) => {
+                console.log(e);
+                toast({
+                    title: "An Error occured",
+                    description: e.message,
+                    status: "error",
+                    variant: "left-accent",
+                    duration: 4000,
+                    isClosable: true,
+                });
+            });
+    }, [router.query.id, toast]);
 
-  // Load training from API
-  const loadTraining = useCallback(() => {
-    return axiosI
-      .get(`/training/${router.query.id}`)
-      .then((res) => {
-        setTraining(res.data);
-      })
-      .catch((e) => {
-        console.log(e);
-        toast({
-          title: "An Error occured",
-          description: e.message,
-          status: "error",
-          variant: "left-accent",
-          duration: 4000,
-          isClosable: true,
+    const loadExerciseEvents = useCallback(() => {
+        return axiosI
+            .get(`/training/${router.query.id}/exercises`)
+            .then((res) => {
+                setExerciseEvents(res.data);
+            })
+            .catch((e) => {
+                console.log(e);
+                toast({
+                    title: "An Error occured",
+                    description: e.message,
+                    status: "error",
+                    variant: "left-accent",
+                    duration: 4000,
+                    isClosable: true,
+                });
+            });
+    }, [router.query.id, toast]);
+
+    // Load training on first render
+    useEffect(() => {
+        setIsLoading(true);
+        if (!router.query.id) return; // Make sure that no call is mady without a training-id
+        loadTraining().then(() => {
+            setIsLoading(false);
+            setTrainingActive(training && !training.endDateTime);
         });
-      });
-  }, [router.query.id, toast]);
+        loadExerciseEvents();
+    }, [router, toast]);
 
-  // Load training on first render
-  useEffect(() => {
-    setIsLoading(true);
-    loadTraining().then(() => {
-      setIsLoading(false);
-    });
-  }, [loadTraining, toast]);
+    // Add or update an exercise to the training
+    const addOrUpdateExerciseInTraining = (exerciseEvent?: ExerciseEvent) => {
+        // If exercise is undefined, create a new empty exercise
 
-  // Add or update an exercise to the training
-  const addOrUpdateExerciseInTraining = (exercise?: Exercise) => {
-    // If exercise is undefined, create a new empty exercise
+        //axiosI.post(`/training/${router.query.id}/exercise/add`, exercise);
 
-    //axiosI.post(`/training/${router.query.id}/exercise/add`, exercise);
+        // For now just wait 1 second and set loadNewExercise to true then false
+        // Just for implementing the skeleton loading
+        setLoadNewExercise(true);
+        console.log(JSON.stringify(training));
+        setTimeout(() => {
+            setLoadNewExercise(false);
+        }, 2000);
+    };
 
-    // For now just wait 1 second and set loadNewExercise to true then false
-    // Just for implementing the skeleton loading
-    setLoadNewExercise(true);
-    setTimeout(() => {
-      setLoadNewExercise(false);
-    }, 2000);
-  }
+    return (
+        <div className="flex flex-col items-center ">
+            <div className="flex w-5/6 flex-col gap-4">
+                {router.query.id}
+                <br />
+                {training?.startDateTime && format(new Date(training.startDateTime), "dd.MM.yyyy HH:mm")}
+                {training?.startDateTime && <PassedTime startTime={new Date(training.startDateTime)} />}
 
-  return (
-    <div className="flex flex-col items-center ">
-      <div className="flex w-5/6 flex-col gap-4">
-        {router.query.id}
-        <br />
-        {training?.startDateTime &&
-          format(new Date(training.startDateTime), "dd.MM.yyyy HH:mm")}
-
-        <AddExerciseComponent onClick={addOrUpdateExerciseInTraining} />
-        {loadNewExercise && <ExerciseSkeleton />}
-        {training.exercises?.map((exercise) => {
-          return <ExerciseComponent key={exercise.id} exercise={exercise} />;
-        })}
-        
-      </div>
-    </div>
-  );
+                {trainingActive && <AddExerciseComponent onClick={addOrUpdateExerciseInTraining} />}
+                {loadNewExercise && <ExerciseSkeleton />}
+                {training.exerciseEvents?.map((exerciseEvent) => {
+                    return <ExerciseComponent key={exerciseEvent.id} exerciseEvent={exerciseEvent} />;
+                })}
+            </div>
+        </div>
+    );
 }
 
-function AddExerciseComponent({onClick} : {onClick: () => void}) {
-  return (
-    <button
-      className="flex h-24 w-full flex-col items-center justify-center
+function AddExerciseComponent({ onClick }: { onClick: () => void }) {
+    return (
+        <button
+            className="flex h-24 w-full flex-col items-center justify-center
     rounded-xl shadow-lg  outline outline-1 outline-slate-300 hover:bg-slate-200"
-    onClick={onClick}
-    >
-      <AddIcon boxSize={"6"} className="" />
-      Add Exercise
-    </button>
-  );
+            onClick={onClick}
+        >
+            <AddIcon boxSize={"6"} className="" />
+            Add Exercise
+        </button>
+    );
 }
 
-function ExerciseComponent(props: { exercise: Exercise }) {
-  return (
-    <div className=" flex h-24 w-full flex-row items-center justify-center rounded-lg bg-slate-50 shadow-lg">
-      {props.exercise.name}
-    </div>
-  );
+function ExerciseComponent(props: { exerciseEvent: ExerciseEvent }) {
+    return (
+        <div className=" flex h-24 w-full flex-row items-center justify-center rounded-lg bg-slate-50 shadow-lg">
+            {props.exerciseEvent.exerciseType.name}
+        </div>
+    );
 }
 
 function ExerciseSkeleton() {
-  return (
-    <Skeleton className="h-24 w-full" rounded={10}>
-      
-    </Skeleton>
-  )
+    return <Skeleton className="h-24 w-full" rounded={10}></Skeleton>;
+}
+
+// TODO: Account for Time Zones
+// ! Currently time difference is 3 hours less than it should be
+function PassedTime({ startTime }: { startTime: Date }) {
+    const [passedMinutes, setPassedMinutes] = useState<any>();
+    const start = useMemo(() => startTime, [startTime]);
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setPassedMinutes(format(new Date() - start, "mm:ss"));
+        }, 1000);
+        return () => clearInterval(interval);
+    }, []);
+
+    return (
+        <div>
+            <TimeIcon /> {passedMinutes}
+        </div>
+    );
 }
