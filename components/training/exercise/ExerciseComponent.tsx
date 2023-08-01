c780f4ad1236d8317d973799f18f6fe19cc379ea
Aronlukas <hafner.emil@gmail.com>
2023-08-01 13:01:13 +0200

    Renamed ExerciseComponent to ExerciseEventComponent

--- components/training/exercise/ExerciseComponent.tsx
+++
@@ -1,9 +0,0 @@
-import { ExerciseEvent } from "@/pages/trainings/[id]";
-
-export default function ExerciseEventComponent(props: { exerciseEvent: ExerciseEvent }) {
-    return (
-        <div className=" flex h-24 w-full flex-row items-center justify-center rounded-lg bg-slate-50 shadow-lg">
-            {props.exerciseEvent.exerciseType?.name}
-        </div>
-    );
-}
---
+++ components/training/exercise/ExerciseEventComponent.tsx
@@ -0,0 +1,16 @@
+import { ExerciseEvent } from "@/pages/trainings/[id]";
+import { useRouter } from "next/router";
+
+export default function ExerciseEventComponent(props: { exerciseEvent: ExerciseEvent } & { onClick: () => void }) {
+    const { id, exerciseType, sets } = props.exerciseEvent;
+    const router = useRouter();
+
+    return (
+        <div
+            className=" flex h-24 w-full flex-row items-center justify-center rounded-lg bg-slate-300 text-lg font-medium shadow-lg hover:cursor-pointer hover:bg-slate-200"
+            onClick={props.onClick}
+        >
+            {exerciseType?.name ? <div>{exerciseType.name}</div> : <div>No exercise type selected</div>}
+        </div>
+    );
+}
