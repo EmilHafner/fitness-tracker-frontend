60ee5af2d2eb95a9db1a85f9d67981274183e03c
Aronlukas <hafner.emil@gmail.com>
2023-08-03 16:48:24 +0200

    Added SetInterface

--- types/global.d.ts
+++ types/global.d.ts
@@ -1,7 +1,14 @@
 declare module "global-types" {
     interface ExerciseType {
-        id: number;
+        id?: number;
         name: string;
         bodypart: string;
+    }
+
+    interface Set {
+        id?: number;
+        reps: number;
+        weight: number;
+        orderNumber: number;
     }
 }
