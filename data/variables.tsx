baae7ab9d13522b3cb5809bcfc0603fb70e0d9aa
Aronlukas <hafner.emil@gmail.com>
2023-08-05 18:55:28 +0200

    Moved itemList to global variables file.

---
+++ data/variables.tsx
@@ -0,0 +1,8 @@
+import { ItemInterface } from "global-types";
+
+
+export const itemList: ItemInterface[] = [
+    { title: "Trainings", href: "/trainings" },
+    { title: "Plans", href: "/plans" },
+    { title: "Exercises", href: "/exercises" },
+];
