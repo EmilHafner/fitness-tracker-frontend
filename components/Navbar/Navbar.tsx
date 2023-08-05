fc6b991c74e5c93a3c1a21a1bb72e8ffa9ad3a59
Aronlukas <hafner.emil@gmail.com>
2023-08-06 00:55:39 +0200

    Added comments and used UserCard. UserCard takes only username but not session.

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -27,42 +27,23 @@ export default function Navbar() {
     return (
         <div className={"relative z-50 mb-6 border-b-2 bg-stone-200"}>
             <Box className={"z-50 flex w-full flex-wrap items-center justify-between bg-stone-100 px-12 py-4 md:px-16"}>
+                {/* Logo */}
                 <Link href={"/"} className={"w-32"}>
                     <Image src={"/logo/logo-no-background.svg"} width={120} height={30} alt={"Logo"} />
                 </Link>
 
+                {/* Navbar Items */}
                 <div className={"hidden gap-8 px-4 py-2 md:flex"}>
                     {itemList.map((item, i) => (
                         <NavbarItem key={i} {...item} />
                     ))}
                 </div>
 
-                <div className={"hidden px-4 py-2 md:flex"}>
-                    {!authPage &&
-                        (session?.user ? (
-                            <div className="flex items-center gap-4">
-                                <p className="text-xl font-bold text-accent">{session?.user?.username}</p>
-                                <div
-                                    className={
-                                        "rounded-2xl bg-accent px-8 py-2 font-bold shadow-lg transition-[1s] hover:cursor-pointer hover:bg-accent-muted"
-                                    }
-                                    onClick={() => signOut()}
-                                >
-                                    Logout
-                                </div>
-                            </div>
-                        ) : (
-                            <div
-                                className={
-                                    "rounded-2xl bg-accent px-8 py-2 font-bold shadow-lg transition-[1s] hover:cursor-pointer hover:bg-accent-muted"
-                                }
-                                onClick={() => signIn()}
-                            >
-                                Login
-                            </div>
-                        ))}
+                <div className="hidden md:inline">
+                    <UserCard authPage={authPage} username={session?.user?.username} />
                 </div>
 
+                {/* Hamburger Menu */}
                 <button className="flex py-2 md:hidden" onClick={() => setMenuOpen(!menuOpen)}>
                     <div className="flex h-8 w-8 items-center justify-center">
                         {menuOpen ? (
@@ -73,9 +54,11 @@ export default function Navbar() {
                     </div>
                 </button>
             </Box>
+
+            {/* Menu on smaller screens */}
             {menuOpen && (
                 <div className="absolute z-10 flex w-screen flex-col items-center border-b-2 bg-stone-100 pb-8 ">
-                    <UserCard authPage={authPage} session={session} />
+                    <UserCard authPage={authPage} username={session?.user?.username} />
                     {itemList.map((item, i) => (
                         <NavbarItem key={i} {...item} className="py-6 text-3xl" />
                     ))}
--- components/Navbar/UserCard.tsx
+++ components/Navbar/UserCard.tsx
@@ -1,10 +1,9 @@
-import { Session } from "next-auth";
 import { signIn, signOut } from "next-auth/react";
 
-export function UserCard({ authPage, session }: { authPage: boolean; session: Session | null }) {
+export function UserCard({ authPage, username }: { authPage: boolean; username?: string | null }) {
     if (authPage) return null;
 
-    if (!session) {
+    if (!username) {
         return (
             <div
                 className={
@@ -20,7 +19,7 @@ export function UserCard({ authPage, session }: { authPage: boolean; session: Se
     return (
         <div className={"px-4 py-2 md:flex"}>
             <div className="flex items-center gap-4">
-                <p className="text-xl font-bold text-accent">{session?.user?.username}</p>
+                <p className="text-xl font-bold text-accent">{username}</p>
                 <div
                     className={
                         "rounded-2xl bg-accent px-8 py-2 font-bold shadow-lg transition-[1s] hover:cursor-pointer hover:bg-accent-muted"
