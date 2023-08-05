e5a73126d86d1d489b32b7d66552fffbaf634243
Aronlukas <hafner.emil@gmail.com>
2023-08-06 00:54:23 +0200

    Changed background-color to stone

--- components/training/exercise/ExerciseEventComponent.tsx
+++ components/training/exercise/ExerciseEventComponent.tsx
@@ -1,5 +1,4 @@
 import { ExerciseEvent } from "@/pages/trainings/[id]";
-import { Divider } from "@chakra-ui/react";
 import { useRouter } from "next/router";
 
 export default function ExerciseEventComponent(props: { exerciseEvent: ExerciseEvent } & { onClick: () => void }) {
@@ -8,7 +7,7 @@ export default function ExerciseEventComponent(props: { exerciseEvent: ExerciseE
 
     return (
         <div
-            className="flex h-24 w-full flex-row items-center justify-center rounded-lg bg-slate-300 text-lg font-medium shadow-lg hover:cursor-pointer hover:bg-slate-200"
+            className="flex h-24 w-full flex-row items-center justify-center rounded-lg bg-stone-50 text-lg font-medium shadow-lg hover:cursor-pointer hover:bg-slate-200"
             onClick={props.onClick}
         >
             {exerciseType?.name ? <div>{exerciseType.name}</div> : <div>No exercise type selected</div>}
