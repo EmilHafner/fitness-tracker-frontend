8d534c5c2d23cde6643a21f4a5a0b86e16dcf196
Aronlukas <hafner.emil@gmail.com>
2023-08-06 00:56:48 +0200

    Layoutchange and changed format

--- components/training/TrainingItem.tsx
+++ components/training/TrainingItem.tsx
@@ -1,125 +1,136 @@
-import { Button } from "@chakra-ui/react";
 import { useBoolean } from "@chakra-ui/hooks";
-import {
-  format,
-  differenceInDays,
-  intervalToDuration,
-  formatDuration,
-} from "date-fns";
+import { format, differenceInDays, intervalToDuration, formatDuration } from "date-fns";
 import { useRouter } from "next/router";
 import { stopTraining } from "@/services/axiosInstance";
 
 export interface TrainingItemInterface {
-  id: number;
-  startDateTime: Date;
-  endDateTime: Date;
+    id: number;
+    startDateTime: Date;
+    endDateTime: Date;
 }
 
-export default function TrainingItem(
-  props: TrainingItemInterface & { reloadItems: () => void }
-) {
-  const router = useRouter();
-  const [loading, setLoading] = useBoolean();
+export default function TrainingItem(props: TrainingItemInterface & { reloadItems: () => void }) {
+    const router = useRouter();
+    const [loading, setLoading] = useBoolean();
 
-  const start: Date = new Date(props.startDateTime);
-  const end: Date | null = props.endDateTime
-    ? new Date(props.endDateTime)
-    : null;
-  const active = !end;
+    const start: Date = new Date(props.startDateTime);
+    const end: Date | null = props.endDateTime ? new Date(props.endDateTime) : null;
+    const active = !end;
 
-  // TODO: Add Icons for records
-  // TODO: Add delete and edit button
-  // TODO: Handle mutliple days differently
-  // TODO: Highlight Trainings which are not completed. And add complete-Button to them
+    // TODO: Add Icons for records
+    // TODO: Add delete and edit button
+    // TODO: Handle mutliple days differently
+    // TODO: Highlight Trainings which are not completed. And add complete-Button to them
 
-  const getStartDateString = (): string => {
-    return format(start, "eeee, MMM do yyyy");
-  };
+    const getStartDateString = (): string => {
+        return format(start, "MMMM do");
+    };
 
-  const getDurationString = (): string => {
-    if (!end) return "";
+    const getDurationString = (): string => {
+        if (!end) return "";
 
-    let d = intervalToDuration({
-      start: start,
-      end: end,
-    });
+        let d = intervalToDuration({
+            start: start,
+            end: end,
+        });
 
-    return formatDuration(d, { format: ["hours", "minutes"] });
-  };
+        return `${d.hours}h ${d.minutes}m`;
+    };
 
-  const getTimeFromTo = (): string => {
-    if (!end) {
-      return format(start, "HH:mm");
-    }
-    if (differenceInDays(end, start) > 1) {
-      return (
-        format(start, "HH:mm") +
-        " to " +
-        format(end, "HH:mm '(different day: ' dd.MM.yyyy')'")
-      );
-    }
-    return format(start, "HH:mm") + " to " + format(end, "HH:mm");
-  };
-
-  const onClick = () => {
-    console.log("Clicked on TrainingItem"); // TODO: Remove this
-    router.push("trainings/" + props.id);
-  };
-
-  const stopThisTraining = (e: React.MouseEvent<any>) => {
-    setLoading.on();
-    e.stopPropagation();
-    stopTraining(props.id).then(() => props.reloadItems());
-    setLoading.off();
-  };
-
-  return (
-      <div
-        onClick={onClick}
-        className={
-          "flex justify-center w-5/6 border-4 border-accent-muted px-16 py-4 rounded-xl hover:cursor-pointer hover:shadow-lg hover:border-accent"
+    const getTimeFromTo = (): string => {
+        if (!end) {
+            return format(start, "HH:mm");
         }
-      >
-        <div className={"flex justify-between w-full"}>
-          <div className="flex">
-            <div
-              className={
-                "relative -left-4 w-2 rounded-md " +
-                (active ? "bg-primary" : "bg-accent")
-              }
-            ></div>
-            <div className={"flex flex-col"}>
-              <div className={"flex items-center pb-1"}>
-                <h2 className={"font-bold pr-10"}>{getStartDateString()}</h2>
-                <h2>{getDurationString()}</h2>
-              </div>
-              <div className="pb-6">
-                <div>{getTimeFromTo()}</div>
-              </div>
-              <div>Icons for records</div>
-            </div>
-          </div>
+        if (differenceInDays(end, start) > 1) {
+            return format(start, "HH:mm") + " to " + format(end, "HH:mm '(different day: ' dd.MM.yyyy')'");
+        }
+        return format(start, "HH:mm") + " to " + format(end, "HH:mm");
+    };
 
-          <div className={"flex flex-col justify-around"}>
-            {active ? (
-              // ! This button doesnt work yet
-              <Button
-                className={
-                  "py-2 px-4 rounded bg-abort hover:bg-abort-muted font-medium"
-                }
-                css={
-                  "py-2 px-4 rounded bg-abort hover:bg-abort-muted font-medium"
-                }
-                onClick={stopThisTraining}
-                disabled={loading}
-              >
-                Stop this training
-              </Button>
-            ) : (
-              <></>
-            )}
-          </div>
+    const onClick = () => {
+        router.push("trainings/" + props.id);
+    };
+
+    const stopThisTraining = (e: React.MouseEvent<any>) => {
+        setLoading.on();
+        e.stopPropagation();
+        stopTraining(props.id).then(() => props.reloadItems());
+        setLoading.off();
+    };
+
+    const getTitle = (): string => {
+        if (!end) {
+            return "Active Training";
+        } else {
+            // Different title for time of day (morning, afternoon, evening)
+            let hour = start.getHours();
+            if (hour < 12) {
+                return "Morning Training";
+            } else if (hour < 18) {
+                return "Afternoon Training";
+            } else {
+                return "Evening Training";
+            }
+        }
+    };
+
+    return (
+        <div
+            onClick={onClick}
+            className={
+                "flex h-full w-5/6 bg-stone-50 rounded-xl border-2 px-8 py-4 shadow-sm hover:cursor-pointer hover:border-accent hover:shadow-lg md:px-16"
+            }
+        >
+            <div className={"mr-2 w-2 rounded-md " + (active ? "bg-primary" : "bg-accent")}></div>
+            <div className="flex w-full flex-col">
+                {/* Data from the training */}
+                <div className={"flex w-full justify-between"}>
+                    <div className="flex">
+                        <div className={"flex flex-col justify-between"}>
+                            <div className={"flex flex-col pb-1"}>
+                                {/* Startdate and Duration */}
+                                <span className={"text-sm font-bold text-stone-800 md:text-lg"}>{getTitle()}</span>
+                                <span className={"text-sm text-stone-600"}>{getStartDateString()}</span>
+                                {/* <span className="text-sm text-stone-800">{getTimeFromTo()}</span> */}
+                            </div>
+                        </div>
+                    </div>
+                    <div className="">
+                        <div className="text-sm font-medium text-stone-800">{getDurationString()}</div>
+                    </div>
+                </div>
+                <div className="py-2">{/* Divider */}</div>
+                <div className="flex w-4/5 sm:w-52 flex-col text-xs text-stone-600 md:w-56 md:text-sm">
+                    <div className="flex justify-between font-bold">
+                        <span>Exercise</span>
+                        <span>Avg. Set</span>
+                    </div>
+                    <div className="flex justify-between">
+                        <span>Exercise name is long</span>
+                        <span>25 kg x 12</span>
+                    </div>
+                    <div className="flex justify-between">
+                        <span>Exercise name</span>
+                        <span>25 kg x 12</span>
+                    </div>
+                    <div className="flex justify-between">
+                        <span>Exercise name</span>
+                        <span>25 kg x 12</span>
+                    </div>
+                    <div className="flex justify-between">
+                        <span>Exercise name</span>
+                        <span>25 kg x 12</span>
+                    </div>
+                    <div className="flex justify-between">
+                        <span>Exercise name</span>
+                        <span>25 kg x 12</span>
+                    </div>
+                    <div className="flex justify-between">
+                        <span>Exercise name</span>
+                        <span>25 kg x 12</span>
+                    </div>
+                </div>
+            </div>
         </div>
-      </div>
-  );
+    );
 }
