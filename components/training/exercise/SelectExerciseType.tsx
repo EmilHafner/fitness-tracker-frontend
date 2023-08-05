4cf6aa9dc3875fdb1cbaaeca5d9291e4138efe30
Aronlukas <hafner.emil@gmail.com>
2023-08-06 00:57:34 +0200

    Put SelectExerciseType into a different file.

---
+++ components/training/exercise/SelectExerciseType.tsx
@@ -0,0 +1,71 @@
+import { useExerciseTypes } from "@/hooks/useExerciseTypes";
+import { ChevronDownIcon } from "@chakra-ui/icons";
+import { Collapse, Input, InputGroup, InputRightElement, useDisclosure, useOutsideClick } from "@chakra-ui/react";
+import { ExerciseType } from "global-types";
+import React from "react";
+
+
+export default function SelectExerciseType({ initialType, exerciseId }: { initialType?: string; exerciseId?: string }) {
+    const { exerciseTypes, updateExerciseTypes, exerciseTypesLoading, saveExerciseType, searchTerm, setSearchTerm } =
+        useExerciseTypes(initialType);
+    const { isOpen, onOpen, onClose } = useDisclosure();
+    const ref = React.useRef<HTMLDivElement>(null);
+
+    useOutsideClick({
+        ref: ref,
+        handler: () => {
+            if (!isOpen) return;
+            onClose();
+            setExerciseType(searchTerm);
+        },
+    });
+
+    const setExerciseType = (exerciseTypeName: string) => {
+        if (!exerciseId) return;
+        if (!exerciseTypes) {
+            setTimeout(() => "", 1000);
+        }
+        if (!exerciseTypes) return;
+        const typeId = exerciseTypes.find((type) => type.name === exerciseTypeName)?.id;
+        if (!typeId) return;
+
+        saveExerciseType(parseInt(exerciseId), typeId);
+    };
+
+    const setOption = (option: string) => {
+        setSearchTerm(option);
+        onClose();
+        setExerciseType(option);
+    };
+
+    return (
+        <div ref={ref} className="relative w-full">
+            <InputGroup onClick={onOpen}>
+                <Input value={searchTerm} onChange={(e) => setSearchTerm(e.target.value)} />
+                <InputRightElement>
+                    <ChevronDownIcon width={6} height={6} />
+                </InputRightElement>
+            </InputGroup>
+            <div className="absolute w-full">
+                <Collapse in={isOpen}>
+                    <div className="flex flex-col items-start bg-white">
+                        {exerciseTypes?.map((type) => {
+                            return <ExerciseTypeOption type={type} key={type.id} setOption={setOption} />;
+                        })}
+                    </div>
+                </Collapse>
+            </div>
+        </div>
+    );
+}
+
+function ExerciseTypeOption({ type, setOption }: { type: ExerciseType; setOption: Function }) {
+    return (
+        <button
+            className="z-10 h-10 w-full rounded p-2 outline outline-1 -outline-offset-2 outline-gray-200 hover:bg-blue-100 focus:outline-2 focus:outline-blue-400"
+            onClick={() => setOption(type.name)}
+        >
+            {type.name}
+        </button>
+    );
+}
--- pages/trainings/exercises/[exerciseId].tsx
+++ pages/trainings/exercises/[exerciseId].tsx
@@ -1,17 +1,5 @@
 import { getExerciseEventById } from "@/services/axiosInstance";
-import {
-    Collapse,
-    Fade,
-    InputGroup,
-    InputRightElement,
-    Select,
-    SlideFade,
-    useDisclosure,
-    useOutsideClick,
-} from "@chakra-ui/react";
-import { ExerciseType } from "global-types";
 import { useRouter } from "next/router";
-import { Input } from "@chakra-ui/react";
 import { useEffect, useState } from "react";
 import React from "react";
 import { getSession } from "next-auth/react";
@@ -19,15 +7,15 @@ import { GetServerSideProps } from "next";
 import { ExerciseEvent } from "../[id]";
 import { useSets } from "@/hooks/useSets";
 import Button from "@/components/basics/Button";
-import { useExerciseTypes } from "@/hooks/useExerciseTypes";
 import LoadingPage from "@/components/loading/LoadingPage";
 import TrainingsSetComponent from "@/components/training/exercise/TrainingsSetComponent";
-import { AddIcon, CheckCircleIcon, ChevronDownIcon } from "@chakra-ui/icons";
+import { AddIcon, CheckCircleIcon } from "@chakra-ui/icons";
+import SelectExerciseType from "@/components/training/exercise/SelectExerciseType";
 
 export default function Exercise() {
     const router = useRouter();
     const [exerciseEvent, setExerciseEvent] = useState<ExerciseEvent>({} as ExerciseEvent);
-    const { sets, addSet, updateSets, setsLoading } = useSets(parseInt(router.query?.exerciseId as string));
+    const { sets, addSet } = useSets(parseInt(router.query?.exerciseId as string));
     const [exerciseEventLoading, setExerciseEventLoading] = useState<boolean>(true);
     const [loadingError, setLoadingError] = useState<boolean>(false);
 
@@ -67,7 +55,7 @@ export default function Exercise() {
                             exerciseId={router.query?.exerciseId as string}
                         />
                     </div>
-                    <Button className="bg-blue-400 hover:bg-blue-300 max-w-sm w-2/5">
+                    <Button className="bg-blue-400 hover:bg-blue-300 max-w-sm w-2/6">
                         <div className="flex items-center gap-2">
                             <CheckCircleIcon />
                             <span className="font-medium">Complete</span>
@@ -97,71 +85,7 @@ export default function Exercise() {
     );
 }
 
-function SelectExerciseType({ initialType, exerciseId }: { initialType?: string; exerciseId?: string }) {
-    const { exerciseTypes, updateExerciseTypes, exerciseTypesLoading, saveExerciseType, searchTerm, setSearchTerm } =
-        useExerciseTypes(initialType);
-    const { isOpen, onOpen, onClose } = useDisclosure();
-    const ref = React.useRef<HTMLDivElement>(null);
-
-    useOutsideClick({
-        ref: ref,
-        handler: () => {
-            if (!isOpen) return;
-            onClose();
-            setExerciseType(searchTerm);
-        },
-    });
-
-    const setExerciseType = (exerciseTypeName: string) => {
-        if (!exerciseId) return;
-        if (!exerciseTypes) {
-            setTimeout(() => "", 1000);
-        }
-        if (!exerciseTypes) return;
-        const typeId = exerciseTypes.find((type) => type.name === exerciseTypeName)?.id;
-        if (!typeId) return;
-
-        saveExerciseType(parseInt(exerciseId), typeId);
-    };
-
-    const setOption = (option: string) => {
-        setSearchTerm(option);
-        onClose();
-        setExerciseType(option);
-    };
-
-    return (
-        <div ref={ref} className="relative w-full">
-            <InputGroup onClick={onOpen}>
-                <Input value={searchTerm} onChange={(e) => setSearchTerm(e.target.value)} />
-                <InputRightElement>
-                    <ChevronDownIcon width={6} height={6} />
-                </InputRightElement>
-            </InputGroup>
-            <div className="absolute w-full">
-                <Collapse in={isOpen}>
-                    <div className="flex flex-col items-start bg-white">
-                        {exerciseTypes?.map((type) => {
-                            return <ExerciseTypeOption type={type} key={type.id} setOption={setOption} />;
-                        })}
-                    </div>
-                </Collapse>
-            </div>
-        </div>
-    );
-}
-
-function ExerciseTypeOption({ type, setOption }: { type: ExerciseType; setOption: Function }) {
-    return (
-        <button
-            className="z-10 h-10 w-full rounded p-2 outline outline-1 -outline-offset-2 outline-gray-200 hover:bg-blue-100 focus:outline-2 focus:outline-blue-400"
-            onClick={() => setOption(type.name)}
-        >
-            {type.name}
-        </button>
-    );
-}
-
+// Make sure the user is logged in before rendering this page.
 export const getServerSideProps: GetServerSideProps = async (context) => {
     const session = await getSession(context);
 
