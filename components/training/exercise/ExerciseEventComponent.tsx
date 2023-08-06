763ba8695d9c8850837126a5462aac9626644e8d
Aronlukas <hafner.emil@gmail.com>
2023-08-06 15:33:03 +0200

    ExerciseEventComponent now looks nicer and displays the trainingsSets.

--- components/training/exercise/ExerciseEventComponent.tsx
+++ components/training/exercise/ExerciseEventComponent.tsx
@@ -2,15 +2,32 @@ import { ExerciseEvent } from "@/pages/trainings/[id]";
 import { useRouter } from "next/router";
 
 export default function ExerciseEventComponent(props: { exerciseEvent: ExerciseEvent } & { onClick: () => void }) {
-    const { id, exerciseType, sets } = props.exerciseEvent;
+    const { id, exerciseType, trainingsSets } = props.exerciseEvent;
     const router = useRouter();
+
+    const sets = trainingsSets || [];
+
 
     return (
         <div
-            className="flex h-24 w-full flex-row items-center justify-center rounded-lg bg-stone-50 text-lg font-medium shadow-lg hover:cursor-pointer hover:bg-slate-200"
+            className="flex w-full flex-col justify-between rounded-lg border-2 bg-stone-50 px-4 py-2 md:px-16 text-sm font-medium hover:cursor-pointer hover:border-accent hover:shadow-lg md:text-base"
             onClick={props.onClick}
         >
-            {exerciseType?.name ? <div>{exerciseType.name}</div> : <div>No exercise type selected</div>}
+            <div className="text-base">
+                {exerciseType?.name ? <div>{exerciseType.name}</div> : <div>No exercise type selected</div>}
+            </div>
+            <div className="flex flex-col text-sm text-stone-700 w-3/5">
+                <div className="flex flex-row justify-between">
+                    <span>Nr.</span> <span>Weight x Reps</span>
+                </div>
+                {sets.map((set, index) => {
+                    return (
+                        <div key={index} className="flex flex-row justify-between text-stone-600 font-normal">
+                            <span>{set.orderNumber}.</span><span>{set.weight} kg x {set.reps}</span>
+                        </div>
+                    )
+                })}
+            </div>
         </div>
     );
 }
