a2db060c4eb455c3ffa2c1eb3fae9d28a7828a26
Aronlukas <hafner.emil@gmail.com>
2023-08-15 16:44:35 +0200

    Added TanStack devtools

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,6 +4,7 @@ import { ChakraProvider } from "@chakra-ui/react";
 import { SessionProvider } from "next-auth/react";
 import Navbar from "@/components/Navbar/Navbar";
 import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 export default function App({ Component, pageProps }: AppProps) {
     const queryClient = new QueryClient();
@@ -16,6 +17,7 @@ export default function App({ Component, pageProps }: AppProps) {
                     <Component {...pageProps} />
                 </ChakraProvider>
             </SessionProvider>
+            <ReactQueryDevtools />
         </QueryClientProvider>
     );
 }
